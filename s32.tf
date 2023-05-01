511fa89af7e59c5f65b472cbad14c31a556c828d
br.vendrametto <brvendrametto@gmail.com>
2023-05-01 16:28:33 -0300

    Ajustes

---
+++ s32.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "codepipeline_artifacts1" {
+  bucket        = "s31-brvendrametto"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "codepipeline_artifacts_ctrl1" {
+  bucket = aws_s3_bucket.codepipeline_artifacts1.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_artifacts_acl1" {
+  depends_on = [aws_s3_bucket_ownership_controls.codepipeline_artifacts_ctrl1]
+
+  bucket = aws_s3_bucket.codepipeline_artifacts1.id
+  acl    = "private"
+}
